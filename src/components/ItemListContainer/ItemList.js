5b6d2b3bac3489e476779c5cfd1eb1ec13f15dac
Guillermo Seclen Alfaro <guille.2602@gmail.com>
2022-12-06 18:44:50 -0300

    Solucionado bug que quedaba en loop la lectura de firebase

--- src/components/ItemListContainer/ItemList.js
+++ src/components/ItemListContainer/ItemList.js
@@ -1,21 +1,23 @@
 import React from "react";
 import Item from "./Item.js";
 import '../../../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import { convertToARSMoneyFormat } from "../../context/cartContext.jsx";
 
-function ItemList () {
-    {products.map(({id, producto, precio, imgurl}) => {
-        return (
-        <Item 
-            className='ms-auto col-sm-12 col-md-4 col-lg-4 col-xl-4'
-            key ={id}
-            id={id}
-            name={producto}
-            price={precio}
-            imgurl={imgurl}>
-        </Item>)
-    })   
-    }
+function ItemList ({ products }) {
+    return(
+        <>
+        {
+        products.map((product) => {
+            return (
+            <Item
+                className='ms-auto col-sm-12 col-md-4 col-lg-4 col-xl-4'
+                key ={product.id}
+                id={product.id}
+                name={product.producto}
+                price={product.precio}
+                imgurl={product.imgurl}>
+            </Item>)
+        })}   
+    </>)
 }
 
 export default ItemList;
--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -2,7 +2,7 @@
 import React from "react";
 import {useEffect, useState} from "react";
 import './ItemListContainer.css'
-import Item from "./Item.js";
+import ItemList from "./ItemList.js";
 import getItems, { getItemsByCategory } from "../../services/firebase.js";
 import '../../../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import { useParams } from 'react-router-dom';
@@ -11,20 +11,21 @@ import { Waveform } from '@uiball/loaders';
 //Renderizar contenedor de cards de productos
 function ItemListContainer ({greetings}) {
 
-    const [products, setProducts] = useState([]);
+    const [products, setProducts] = useState(null);
     const [loading, setLoading] = useState(true)
     const { categoryid } = useParams();
 
     async function loadProducts() {
-        
-        let prods = categoryid === undefined
+        let prods = !categoryid
         ? await getItems()
         : await getItemsByCategory(categoryid);
         setProducts(prods);
         setLoading(false);
     }
-    // eslint-disable-next-line 
-    useEffect(()=>{loadProducts()},[categoryid]);
+
+    useEffect(() => {
+        loadProducts();
+    }, [categoryid]);
 
     if (loading) return (
         <div className="anim-position">
@@ -41,19 +42,7 @@ function ItemListContainer ({greetings}) {
         <div className="ms-auto box">
             <h1 className="mainTitle">{greetings}</h1>
             <div className='prodsBox'>
-  
-            {products.map(({id, producto, precio, imgurl}) => {
-                return (
-                <Item 
-                    className='ms-auto col-sm-12 col-md-4 col-lg-4 col-xl-4'
-                    key ={id}
-                    id={id}
-                    name={producto}
-                    price={precio}
-                    imgurl={imgurl}>
-                </Item>)
-            })   
-            }
+                <ItemList products={products}/>
             </div>
         </div>
     );
